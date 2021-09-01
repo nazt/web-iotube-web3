1e5ab7babed6ff7e063c23026a3180ce31eb6820
Yan Tao <yan.tao.work@gmail.com>
2021-09-01 21:07:56 +0200

    update total value locked

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -39,7 +39,7 @@ export const Home = observer(() => {
               <StatLabel>{lang.t('info.assets')}</StatLabel>
             </Stat>
             <Stat>
-              <StatNumber fontSize="2xl" color={textColor} fontWeight={100}>$20,000,000+</StatNumber>
+              <StatNumber fontSize="2xl" color={textColor} fontWeight={100}>$50,000,000+</StatNumber>
               <StatLabel>{lang.t('info.total_value_locked')}</StatLabel>
             </Stat>
           </StatGroup>

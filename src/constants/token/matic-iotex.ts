701ce042b880a6bcb0bb11c2f3443804b9160428
anshuomomo <shuo@iotex.io>
2021-09-07 14:18:57 +0800

    matic tokens name remove -matic

--- src/constants/token/matic-iotex.ts
+++ src/constants/token/matic-iotex.ts
@@ -33,7 +33,7 @@ export const polygonToIotexTokens = {
       destAddress: '0x7f0ad63c902c67b1fa1b1102b0daffb889f5d5cb',
       name: 'Wrapped BTC',
       decimals: 8,
-      symbol: 'WBTC-matic',
+      symbol: 'WBTC',
       logoURI: 'https://iotexscan.io/image/token/io10u9dv0ys93nmr7smzyptpkhlhzylt4wtqzrvld.png'
     },
     {
@@ -41,14 +41,14 @@ export const polygonToIotexTokens = {
       destAddress: '0x62a9d987cbf4c45a550deed5b57b200d7a319632',
       name: 'DAI Stablecoin',
       decimals: 18,
-      symbol: 'DAI-matic',
+      symbol: 'DAI',
       logoURI: 'https://iotexscan.io/image/token/io1v25anp7t7nz954gdam2m27eqp4arr93jly4maa.png'
     }, {
       address: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F',
       destAddress: '0x3cdb7c48e70b854ed2fa392e21687501d84b3afc',
       name: 'Tether USD',
       decimals: 6,
-      symbol: 'USDT-matic',
+      symbol: 'USDT',
       logoURI: 'https://iotexscan.io/image/token/io18ndhcj88pwz5a5h68yhzz6r4q8vykwhugq45ns.png'
     },
     {
@@ -56,34 +56,34 @@ export const polygonToIotexTokens = {
       destAddress: '0xc04da3a99d17135857bb937d2fbb321d3b6c6a81',
       name: 'USD Coin',
       decimals: 6,
-      symbol: 'USDC-matic',
+      symbol: 'USDC',
       logoURI: 'https://iotexscan.io/image/token/io1cpx682vazuf4s4amjd7jlwejr5akc65plgmdru.png'
     }, {
       address: '0x0b3F868E0BE5597D5DB7fEB59E1CADBb0fdDa50a',
       destAddress: '0x295ebb8c782e186bcb70d9a8124053043d1adf5c',
       name: 'SushiToken',
       decimals: 18,
-      symbol: 'Sushi-matic',
+      symbol: 'Sushi',
       logoURI: 'https://iotexscan.io/image/token/io1990thrrc9cvxhjmsmx5pysznqs734h6uhxfxkq.png'
     }, {
       address: '0x831753DD7087CaC61aB5644b308642cc1c33Dc13',
       destAddress: '0xe46ba98a87dca989725e9a2389975c0bbbb8f985',
       name: 'Quickswap',
       decimals: 18,
-      symbol: 'QUICK-matic',
+      symbol: 'QUICK',
       logoURI: 'https://iotexscan.io/image/token/io1u346nz58mj5cjuj7ng3cn96upwam37v9axtju2.png'
     }, {
       address: '0xD6DF932A45C0f255f85145f286eA0b292B21C90B',
       name: 'Aave Token',
       decimals: 18,
-      symbol: 'AAVE-matic',
+      symbol: 'AAVE',
       logoURI: 'https://iotexscan.io/image/token/io14tw8gyn3p8v5fcmvh4c0w8797ryjrlrvnsr6zf.png'
     }, {
       address: '0xcFb54a6D2dA14ABeCD231174FC5735B4436965D8',
       destAddress: '0x4d7b88403aa2f502bf289584160db01ca442426c',
       name: 'Cyclone Protocol',
       decimals: 18,
-      symbol: 'CYC-matic',
+      symbol: 'CYC',
       logoURI: 'https://iotexscan.io/image/token/io1f4acssp65t6s90egjkzpvrdsrjjyysnvxgqjrh.png'
     },
     {

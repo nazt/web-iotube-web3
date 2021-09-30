02c4b0ec3fab99920e474fedc8e3fda61892b96b
Solan <anshuomomo@gmail.com>
2021-09-30 17:17:29 +0800

    Update iotex-pairs.ts

--- src/constants/ccToken/iotex-pairs.ts
+++ src/constants/ccToken/iotex-pairs.ts
@@ -5,6 +5,12 @@ export const iotexCCSwapPairs = {
     decimals: 18,
     symbol: 'WIOTX',
     logoURI: 'https://iotexproject.iotex.io/iotex-token-metadata/master/images/io15qr5fzpxsnp7garl4m7k355rafzqn8grrm0grz.png'
+  }, {
+    address: '0xa00744882684c3e4747faefd68d283ea44099d03',
+    name: 'iMAGIC Token',
+    decimals: 18,
+    symbol: 'iMAGIC',
+    logoURI: 'https://raw.githubusercontent.com/magiclandfinance/logo/master/logo.png'
   }],
   ccTokens: [{
     name: 'Crosschain IOTX',
@@ -12,5 +18,11 @@ export const iotexCCSwapPairs = {
     address: '0x99B2B0eFb56E62E36960c20cD5ca8eC6ABD5557A',
     decimals: 18,
     logoURI: '/images/tokens/ctoken_logo.jpeg'
+  }, {
+    address: '0xc1932AC6f0aD660bE341B5C980E44df31a30f528',
+    name: 'Cross Chain iMAGIC',
+    decimals: 18,
+    symbol: 'CiMAGIC',
+    logoURI: '/images/tokens/token_imagic-c.png'
   }]
 };
